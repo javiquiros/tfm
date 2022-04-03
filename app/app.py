4a26e3f2e676e68e7547093f3334bee64a27a94b
Javier Quirós <javiquiros@hotmail.com>
2022-04-03 23:44:58 +0200

    Add dashboard with styles

--- app/app.py
+++ app/app.py
@@ -3,12 +3,20 @@ from flask import Flask, render_template
 from app.mongo_client import db
 from app.pokemon import pokedex
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path="",
+            static_folder='static',
+            template_folder='templates')
 
 
 @app.route("/")
 def index():
-    return render_template('bootstrap_table.html', title='Pokemon collection',
+    return render_template('dashboard.html', title='Pokemon collection')
+
+
+@app.route("/pokemon/list")
+def pokemon():
+    return render_template('pokemon_table.html', title='Pokemon collection',
                            pokemon_coll=db.pokemon.find().sort("_id").limit(5))
 
 
