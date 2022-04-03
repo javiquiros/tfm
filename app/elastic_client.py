1dc5a724ffff262480d1ee9b3809ec5990226d22
Javier Quirós <javiquiros@hotmail.com>
2022-04-04 00:23:29 +0200

    Move elastic client

--- app/elastic_client.py
+++ app/elastic_client.py
@@ -3,7 +3,7 @@ from elasticsearch import Elasticsearch
 
 class ElasticClient:
     def __init__(self):
-        self.host = "http://localhost:9200"
+        self.host = "http://elasticsearch:9200"
         self.index = "pokemon"
         self.user = "elastic"
         self.password = "secret"
@@ -21,6 +21,11 @@ class ElasticClient:
     def get_all(self):
         resp = self.es.search(index=self.index, query={"match_all": {}})
         return resp['hits']['hits']
+
+    def count_documents(self):
+        self.index_document({"id": 1, "test": "test"})
+        self.es.indices.refresh(index=self.index)
+        return self.es.count(index=self.index)["count"]
 
     def delete_index(self):
         self.es.indices.delete(index=self.index)
